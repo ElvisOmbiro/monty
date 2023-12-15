8b313501e9b4eebc2d57df38dd9372e3b1fc80c6
ElvisOmbiro <elvisombiro99@gmail.com>
2023-12-15 10:58:35 -0800

    frees_stack

--- mainmonty.c
+++ mainmonty.c
@@ -46,24 +46,3 @@ int main(int argc, char *argv[])
 
 	return (0);
 }
-
-/**
- * frees_stack - free the stack
- * @stack: ptr to stack
- * Return: Nothing
- */
-void frees_stack(stack_t **stack)
-{
-	stack_t *head = *stack;
-
-	while (head)
-	{
-		if (!head->next)
-		{
-			free(head);
-			break;
-		}
-		head = head->next;
-		free(head->prev);
-	}
-}
